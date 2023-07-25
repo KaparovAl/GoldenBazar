77be910844062ce262990a265638f5b092f5ad27
KaparovAl <140455987+KaparovAl@users.noreply.github.com>
2023-07-25 10:41:59 +0300

    Update sw.js

--- restaraunt/sw.js
+++ restaraunt/sw.js
@@ -1,19 +1,3 @@
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
 // Caches
 var CURRENT_CACHES = {
     font: 'font-cache-v1',
